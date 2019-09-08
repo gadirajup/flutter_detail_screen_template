8a060ebff8fa7efceabd8c72d1066337beb9c97d
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-09-08 15:02:26 -0700

    init

---
+++ detail_screen_template/lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'screens/detail/detail.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Detail(),
+    );
+  }
+}
